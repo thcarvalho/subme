8ce8a36f0edcd39d159e6607535147dd7d8d7abb
Marcelo Batista <61668735+Marcelo-Batista@users.noreply.github.com>
2022-04-20 01:25:52 -0300

    Add files via upload
    commit  inicial

---
+++ subme/src/main/java/com/fatec/lab/eng/subme/SubmeApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.lab.eng.subme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmeApplication.class, args);
+	}
+
+}
---
+++ subme/src/test/java/com/fatec/lab/eng/subme/SubmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.lab.eng.subme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
