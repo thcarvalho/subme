1742ae1bd4a290769643056a505271fe8f4f41f9
Marcelo Batista <marcelo.silva238@fatec.sp.gov.br>
2022-05-06 17:19:05 -0300

    task-001: la vamos nós.

--- subme/src/main/java/com/fatec/lab/eng/subme/dto/Adress.java
+++ subme/src/main/java/com/fatec/lab/eng/subme/dto/Adress.java
@@ -3,65 +3,40 @@ package com.fatec.lab.eng.subme.dto;
 public class Adress extends Register{
 	private String street;
 	private int number;
-	private String additionalAdress;
 	private String city;
-	private String state;
-	private String country;
+	private String State;
 	private String zipcode;
 
 	public String getStreet() {
 		return street;
 	}
-
 	public void setStreet(String street) {
 		this.street = street;
 	}
-
 	public int getNumber() {
 		return number;
 	}
-
 	public void setNumber(int number) {
 		this.number = number;
 	}
-
-	public String getAdditionalAdress() {
-		return additionalAdress;
-	}
-
-	public void setAdditionalAdress(String additionalAdress) {
-		this.additionalAdress = additionalAdress;
-	}
-
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
 	public String getState() {
-		return state;
+		return State;
 	}
-
 	public void setState(String state) {
-		this.state = state;
+		State = state;
 	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
 	public String getZipcode() {
 		return zipcode;
 	}
-
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
 	}
+
+
 }
